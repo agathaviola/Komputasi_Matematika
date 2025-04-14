6a5ee01f1f9bc46dfe7704c34f94881a545e4ce2
unknown <agathaviola2307@gmail.com>
2025-04-14 20:54:53 +0700

    Praktikum 1 dan 2 komputasi

---
+++ Praktikum_2.m
@@ -0,0 +1,19 @@
+clc
+clear
+%menghitung luas persegi panjang
+p = 10 ;
+l = 20;
+lp = p*l;
+%disp(lp)
+
+lp1 = p + 1;
+lp2 = p - 1;
+lp3 = p/1;
+lp4 = p*l-1+3;
+
+disp(lp1)
+disp(lp2)
+disp(lp3)
+disp(lp4)
+
+
